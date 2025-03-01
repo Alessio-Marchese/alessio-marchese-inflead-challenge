9da258ee3cc232e46fa79e0bea5b3661bd8c7f31
Alessio Marchese <alessio.marchese148@gmail.com>
2025-03-01 22:15:32 +0100

    Aggiunto mappers

---
+++ Backend/Mappers/UserMapper.cs
@@ -0,0 +1,61 @@
+﻿using Azure.Identity;
+using Backend.DTO.EXAPI;
+using Backend.DTO.MYAPI;
+using Backend.Entities;
+
+namespace Backend.Mappers;
+
+public class UserMapper
+{
+    public static MyApiUserDTO DbToMyApiDTO(User user)
+    {
+        return new MyApiUserDTO()
+        {
+            Id = user.Id.ToString(),
+            Email = user.Email,
+            Username = user.Username,
+            FullName = user.FullName,
+            ProfilePicUrl = user.ProfilePicUrl,
+            Gender = user.Gender,
+            PhoneNumber = user.PhoneNumber,
+            Employment = user.Employment,
+            KeySkill = user.KeySkill,
+            AddressId = user.Address.Id
+        };
+    }
+
+    public static MyApiUserDTO ExapiToMyApiDTO(ExapiUserDTO user)
+    {
+        return new MyApiUserDTO()
+        {
+            Id = user.Id.ToString(),
+            Email = user.Email,
+            Username = user.Username,
+            FullName = $"{user.FirstName} {user.LastName}",
+            ProfilePicUrl = user.ProfilePicUrl,
+            Gender = user.Gender,
+            PhoneNumber = user.PhoneNumber,
+            Employment = $"{user.Employment.Title} {user.Employment.KeySkill}",
+            KeySkill = user.Employment.KeySkill
+            //Manca l'id dell'indirizzo
+        };
+
+    }
+
+    public static User ExapiToDb(ExapiUserDTO user)
+    {
+        return new User()
+        {
+            Uid = user.Uid,
+            Id = user.Id,
+            Email = user.Email,
+            Username = user.Username,
+            FullName = $"{user.FirstName} {user.LastName}",
+            ProfilePicUrl = user.ProfilePicUrl,
+            Gender = user.Gender,
+            PhoneNumber = user.PhoneNumber,
+            Employment = $"{user.Employment.Title} {user.Employment.KeySkill}",
+            KeySkill = user.Employment.KeySkill
+        };
+    }
+}
