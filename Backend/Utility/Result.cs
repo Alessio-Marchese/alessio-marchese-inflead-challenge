49ebaee9cadef40a44098fd4a9d62e9c1c20b339
Alessio Marchese <alessio.marchese148@gmail.com>
2025-03-04 21:43:34 +0100

    Aggiunto Utility per gestire i risultati dei service in maniera piú chiara

---
+++ Backend/Utility/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace Backend.Utility
+{
+    public class Result<T>
+    {
+        public bool IsOk { get; }
+        public T? Data { get; }
+        public string? ErrorMessage { get; }
+
+        private Result(T data)
+        {
+            IsOk = true;
+            Data = data;
+        }
+
+        private Result(string errorMessage)
+        {
+            IsOk = false;
+            ErrorMessage = errorMessage;
+        }
+
+        public static Result<T> Success(T data)
+        {
+            return new Result<T>(data);
+        }
+        public static Result<T> Failure(string errorMessage)
+        {
+            return new Result<T>(errorMessage);
+        }
+    }
+}
